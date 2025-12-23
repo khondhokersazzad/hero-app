bda2e9fe773c591d6635cfa38848bf598b1bcf71
khondhokersazzad <sazzadul.211131@s.pust.ac.bd>
2025-12-23 10:17:43 +0600

    Added Router Setup

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,8 @@ import {
 } from "react-router";
 import HomePageLayout from "../layout/HomePageLayout";
 import Home from "../pages/Home";
+import Apps from "../pages/Apps";
+import Install from "../pages/Install";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +16,23 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>
       },
+
+      {
+        path: "/apps",
+        element: <Apps></Apps>
+      },
+
+      {
+        path: "/apps",
+        element: <Apps></Apps>
+      },
+
+      {
+        path: "/install",
+        element: <Install></Install>
+      },
+
+      
     ]
     
   },
